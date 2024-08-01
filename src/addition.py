fe894389c494cd5212e3a4e812d7c5d7de6c8b2e
jayasurya2116 <69743788+jayasurya2116@users.noreply.github.com>
2024-08-01 19:39:07 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,7 +1,7 @@
-#first commit
-def add(a,b)
+#code updated
+def add(a, b):
     return a + b
 
-def test_add()
-    assert add(1,2) == 3
-    assert add(1,-1) == 0
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
